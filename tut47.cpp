48db7583b210af7147c0487aae008ba0a5590de4
RohanRameshRaut <rohanraut918@gmail.com>
2024-03-30 11:56:51 +0530

    Update tut47.cpp

--- tut47.cpp
+++ tut47.cpp
@@ -16,45 +16,52 @@ Q4. How is code reusability implemented?
 
 // Answer>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-class simpleCalculator{
+class simpleCalculator
+{
     int a, b;
-    public:
-    void getDataSimple(){ // utility function for taking input from user.
-        cout<<"Enter the value of a: "<<endl;
-        cin>>a;
-        cout<<"Enter the value of b: "<<endl;
-        cin>>b;
+
+public:
+    void getDataSimple()
+    { // utility function for taking input from user.
+        cout << "Enter the value of a: " << endl;
+        cin >> a;
+        cout << "Enter the value of b: " << endl;
+        cin >> b;
     }
 
-    void performOperationsSimple(){
-        cout<<"The value of a + b is: "<<a + b<<endl;
-        cout<<"The value of a - b is: "<<a - b<<endl;
-        cout<<"The value of a * b is: "<<a * b<<endl;
-        cout<<"The value of a / b is: "<<a / b<<endl;
+    void performOperationsSimple()
+    {
+        cout << "The value of a + b is: " << a + b << endl;
+        cout << "The value of a - b is: " << a - b << endl;
+        cout << "The value of a * b is: " << a * b << endl;
+        cout << "The value of a / b is: " << a / b << endl;
     }
 };
 
-class scientificCalculator{
-        int a, b;
-    public:
-    void getDataScientific(){ // utility function for taking input from user.
-        cout<<"Enter the value of a: "<<endl;
-        cin>>a;
-        cout<<"Enter the value of b: "<<endl;
-        cin>>b;
+class scientificCalculator
+{
+    int a, b;
+
+public:
+    void getDataScientific()
+    { // utility function for taking input from user.
+        cout << "Enter the value of a: " << endl;
+        cin >> a;
+        cout << "Enter the value of b: " << endl;
+        cin >> b;
     }
 
-    void performOperationsScientific(){
-        cout<<"The value of cos(a) is: "<<cos(a)<<endl;
-        cout<<"The value of sin(a) is: "<<sin(a)<<endl;
-        cout<<"The value of exp(a) is: "<<exp(a)<<endl;
-        cout<<"The value of tan(a) is: "<<tan(a)<<endl;
+    void performOperationsScientific()
+    {
+        cout << "The value of cos(a) is: " << cos(a) << endl;
+        cout << "The value of sin(a) is: " << sin(a) << endl;
+        cout << "The value of exp(a) is: " << exp(a) << endl;
+        cout << "The value of tan(a) is: " << tan(a) << endl;
     }
 };
 
 class hybridCalculator : public simpleCalculator, public scientificCalculator
 {
-
 };
 
 int main()
@@ -64,6 +71,6 @@ int main()
     calc.performOperationsScientific();
     calc.getDataSimple();
     calc.performOperationsSimple();
-    
+
     return 0;
 }
