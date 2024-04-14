91d419e0178c112b4d4ea40a2884259efeee9157
RohanRameshRaut <rohanraut918@gmail.com>
2024-04-14 15:04:41 +0530

    Update vedB.cpp

--- vedB.cpp
+++ vedB.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 /*
@@ -6,43 +7,108 @@ B) Create a C++ class MyString with data members a character pointer and str_len
 */
 class MyString
 {
-    char *ptr1 = new char[100];
-    char *ptr2 = new char[100];
+private:
+    char *str;
     int str_length;
 
 public:
-    MyString(char p1[], char p2[])
+    // Constructor
+    MyString(const char *s)
     {
-        strcpy(ptr1, p1);
-        strcpy(ptr2, p2);
-    }
-    void operator!()
-    {
-        strrev(ptr1);
-        strrev(ptr2);
-    }
-    void operator<(char ptr2[])
-    {
-        cout << "lenght of string 1: " << strlen(ptr1) << endl;
-        cout << "lenght of string 2: " << strlen(ptr2) << endl;
-    }
-    void display()
-    {
-        cout<<ptr1<<endl;
-        cout<<ptr2<<endl;
+        str_length = strlen(s);
+        str = new char[str_length + 1];
+        strcpy(str, s);
     }
 
+    // Destructor
+    ~MyString()
+    {
+        delete[] str;
+    }
+
+    // Function to reverse the case of each alphabet
+    MyString operator!()
+    {
+        char *new_str = new char[str_length + 1];
+        for (int i = 0; i < str_length; i++)
+        {
+            if (isalpha(str[i]))
+            {
+                if (islower(str[i]))
+                {
+                    new_str[i] = toupper(str[i]);
+                }
+                else
+                {
+                    new_str[i] = tolower(str[i]);
+                }
+            }
+            else
+            {
+                new_str[i] = str[i];
+            }
+        }
+        new_str[str_length] = '\0';
+        return MyString(new_str);
+    }
+
+    // Function to compare lengths of two strings
+    bool operator<(const MyString &other)
+    {
+        return str_length < other.str_length;
+    }
+
+    // Function to add constant 'n' to each alphabet of string
+    MyString operator+(int n)
+    {
+        char *new_str = new char[str_length + 1];
+        for (int i = 0; i < str_length; i++)
+        {
+            if (isalpha(str[i]))
+            {
+                new_str[i] = str[i] + n;
+            }
+            else
+            {
+                new_str[i] = str[i];
+            }
+        }
+        new_str[str_length] = '\0';
+        return MyString(new_str);
+    }
+
+    // Function to display the string
+    void display()
+    {
+        cout << str << endl;
+    }
 };
 
 int main()
 {
-    char s1[] = "hello";
-    char s2[] = "world";
-    MyString obj1(s1, s2);
-    !obj1;
-    obj1.display();
-    obj1.operator<(s2);
-    obj1.display();
+    MyString str1("Hello");
+    MyString str2("World");
+
+    cout << "Original strings:" << endl;
+    str1.display();
+    str2.display();
+
+    cout << "\nAfter reversing the case of each alphabet in string 1:" << endl;
+    (!str1).display();
+
+    cout << "\nComparing lengths of two strings: ";
+    if (str1 < str2)
+    {
+        cout << "String 1 is shorter than String 2" << endl;
+    }
+    else
+    {
+        cout << "String 1 is not shorter than String 2" << endl;
+    }
+
+    int n = 1;
+    cout << "\nAdding constant " << n << " to each alphabet in string 2:" << endl;
+    (str2 + n).display();
 
     return 0;
 }
